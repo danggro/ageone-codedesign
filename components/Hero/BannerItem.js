82ec8ef28f00d55f34e441c850a3a4877a53d601
danggro <anangdwin55@gmail.com>
2023-05-02 19:36:05 +0700

    solve bug

--- components/Hero/BannerItem.js
+++ components/Hero/BannerItem.js
@@ -2,7 +2,7 @@ export default function BannerItem(props) {
   return (
     <div
       key={props.key}
-      className="flex flex-col items-center w-[327px] flex-shrink-0 sm:w-4/12"
+      className="flex flex-col items-center w-[327px] sm:flex-shrink flex-shrink-0 sm:w-4/12"
     >
       {props.icon}
       <span className="text-2xl font-medium text-white mt-[10px]">
